c2aca6b0234946a78abd3df9454565170afe7ff7
Ryujinosorus <naim_es-sebbani@ens.univ-artois.fr>
2023-11-27 23:51:42 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ResearchComponent} from "./research/research.component";
+import {PeopleComponent} from "./people/people.component";
+import {PublicationComponent} from "./publication/publication.component";
+import {MeetingComponent} from "./meeting/meeting.component";
+import {GithubComponent} from "./github/github.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'researchs', component: ResearchComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'publications', component: PublicationComponent },
+  { path: 'meetings', component: MeetingComponent },
+  { path: 'github', component: GithubComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
+import { PeopleComponent } from './people/people.component';
+import { PublicationComponent } from './publication/publication.component';
+import { MeetingComponent } from './meeting/meeting.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResearchComponent,
+    PeopleComponent,
+    PublicationComponent,
+    MeetingComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule,
